58fd4a7b4674119f4f66c6be84420728cb412a58
AlexeiLDD <doroga218@gmail.com>
2024-04-02 18:25:49 +0300

    2 - cart added cart

--- public/pages/cart/cart.js
+++ public/pages/cart/cart.js
@@ -30,35 +30,142 @@ export class Cart {
   }
 
   /**
+   *
+   */
+  #addListeners() {
+    const allCheckbox = this.#element.querySelector('[name="cartBlocks"]');
+    const ads = this.#element.querySelectorAll('[name="product"]');
+
+    this.#addCheckboxesListener(allCheckbox, ads);
+
+    const quantity = this.#element.querySelector('[id="quantity"]');
+    const headQuantity = this.#element.querySelector('.basket__quantity');
+    const priceSum = this.#element.querySelector('[id="priceSum"]');
+
+    const des = this.#element.querySelectorAll('.cart-block__item-delete-icon');
+
+    this.#addDeleteListener(des, quantity, headQuantity, priceSum);
+
+    const button = this.#element.querySelector('.selection-panel__action');
+
+    this.#addDeleteCheckedListener(button, quantity, headQuantity, priceSum);
+  }
+
+  /**
+   *
+   * @param {HTMLElement} allCheckbox
+   * @param {NodeListOf} ads
+   */
+  #addCheckboxesListener(allCheckbox, ads) {
+    // const allCheckbox = this.#element.querySelector('[name="cartBlocks"]');
+    // const ads = this.#element.querySelectorAll('[name="product"]');
+
+    allCheckbox.addEventListener('change', () => {
+      ads.forEach((checkbox) => {
+        checkbox.checked = allCheckbox.checked;
+      });
+    });
+
+    ads.forEach((checkbox) => {
+      checkbox.addEventListener('change', () => {
+        if (!checkbox.checked) {
+          allCheckbox.checked = false;
+        } else {
+          allCheckbox.checked = Array.from(ads).every((chbox) => chbox.checked);
+        }
+      });
+    });
+  }
+
+  /**
+   *
+   * @param {HTMLElement} button
+   * @param {HTMLElement} quantity
+   * @param {HTMLElement} headQuantity
+   * @param {HTMLElement} priceSum
+   */
+  #addDeleteCheckedListener(button, quantity, headQuantity, priceSum) {
+    // const button = this.#element.querySelector('.selection-panel__action');
+    // const quantity = this.#element.querySelector('[id="quantity"]');
+    // const headQuantity = this.#element.querySelector('.basket__quantity');
+    // const priceSum = this.#element.querySelector('[id="priceSum"]');
+
+    button.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      const ads = this.#element.querySelectorAll('[name="product"]');
+
+      for (const element of ads) {
+        if (!element.checked) {
+          continue;
+        }
+        const id = element.value;
+        const advert = this.#element.querySelector(`[id="${id}"]`);
+        advert.remove();
+        const price = Number(advert.querySelector('.price').innerHTML);
+        priceSum.innerHTML = Number(priceSum.innerHTML) - price;
+        quantity.innerHTML = Number(quantity.innerHTML) - 1;
+        headQuantity.innerHTML = quantity.innerHTML;
+      }
+    });
+  }
+
+  /**
+   *
+   * @param {NodeListOf} ads
+   * @param {HTMLElement} quantity
+   * @param {HTMLElement} headQuantity
+   * @param {HTMLElement} priceSum
+   */
+  #addDeleteListener(ads, quantity, headQuantity, priceSum) {
+    // const quantity = this.#element.querySelector('[id="quantity"]');
+    // const headQuantity = this.#element.querySelector('.basket__quantity');
+    // const priceSum = this.#element.querySelector('[id="priceSum"]');
+
+    for (const element of ads) {
+      element.addEventListener('click', (ev) => {
+        const id = element.dataset.id;
+        const advert = this.#element.querySelector(`[id="${id}"]`);
+        const price = Number(advert.querySelector('.price').innerHTML);
+        priceSum.innerHTML = Number(priceSum.innerHTML) - price;
+        quantity.innerHTML = Number(quantity.innerHTML) - 1;
+        headQuantity.innerHTML = quantity.innerHTML;
+        advert.remove();
+      });
+    }
+  }
+
+  /**
    * Render a template for a main page.
    */
-  #renderTemplate() {
+  async #renderTemplate() {
     this.#element.appendChild(this.header.render());
-
-    ajax.get(
+    let adverts ={};
+    await ajax.get(
         ajax.routes.cartList,
         (body) => {
-          const adverts = body['items'];
-
-          if (!(adverts && Array.isArray(adverts))) {
-            return;
-          }
-          const quantity = adverts.length;
-          this.#element.innerHTML += renderCartMain(quantity);
-
-          const selectPanel = this.#element.querySelector('.selection-panel');
-          let priceSum = 0;
-
-          adverts.forEach((advert) => {
-            const {price, title} = advert;
-            priceSum += Number(price);
-            selectPanel.innerHTML += renderCartBlock(title, price);
-          });
-
-          const mainCont = this.#element.querySelector('.main-content');
-
-          mainCont.innerHTML += renderSidebar(quantity, priceSum);
+          adverts = body['items'];
         },
     );
+    if (!(adverts && Array.isArray(adverts))) {
+      return;
+    }
+    const quantity = adverts.length;
+    this.#element.innerHTML += renderCartMain(quantity);
+
+    const selectPanel = this.#element.querySelector('.selection-panel');
+    let priceSum = 0;
+
+    adverts.forEach((advert) => {
+      const {id, price, title} = advert;
+      priceSum += Number(price);
+      selectPanel.innerHTML += renderCartBlock(id, title, price);
+    });
+
+    const mainCont = this.#element.querySelector('.main-content');
+
+    mainCont.innerHTML += renderSidebar(quantity, priceSum);
+
+
+    this.#addListeners();
   }
 }
