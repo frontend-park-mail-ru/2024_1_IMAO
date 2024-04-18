9063cd0f50dd303f3909f079db6d0afdc95e1a5f
AlexeiLDD <doroga218@gmail.com>
2024-04-18 14:24:03 +0300

    commit to delete

--- server/index.js
+++ server/index.js
@@ -6,31 +6,28 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(path.resolve(__dirname, '..', 'sw.js'), {
-  maxAge: '31557600', // uses milliseconds per docs
-}));
 app.use('*/public', express.static(path.resolve(__dirname, '..', 'public'), {
-  maxAge: '31557600', // uses milliseconds per docs
+  maxAge: '60000', // uses milliseconds per docs
 }));
 app.use('*/images', express.static(path.resolve(__dirname, 'images'), {
-  maxAge: '31557600', // uses milliseconds per docs
+  maxAge: '60000', // uses milliseconds per docs
 }));
 
 const port = process.env.PORT || 8008;
 
-app.get(/^\/(?!.*\.js$).*$/, function(request, response) {
+app.get(/^\/(?!.*\.(js|ttf)$).*$/, function(request, response) {
   response.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-  response.set('Cache-Control', 'public, max-age=31557600');
+  response.set('Cache-Control', 'public, max-age=60000');
 });
 
-app.get('/sw.js', function(request, response) {
+app.get('*/sw.js', function(request, response) {
   response.sendFile(path.resolve(__dirname, '..', 'public', 'sw.js'));
-  response.set('Cache-Control', 'public, max-age=31557600');
+  response.set('Cache-Control', 'public, max-age=60000');
 });
 
-
-app.get('/test/:num/', function(req, res) {
-  res.send(req.params.num);
+app.get('*/fa3a05a92cf678856b1f.ttf', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '..', 'public', 'fa3a05a92cf678856b1f.ttf'));
+  response.set('Cache-Control', 'public, max-age=60000');
 });
 
 app.listen(port, function() {
