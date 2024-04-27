c53eed44e020bcb690c6e15fa962ba1273a64f48
MaratKamalovPD <dead_island_hd@bk.ru>
2024-04-27 17:34:25 +0300

    deploy

--- public/config/config.js
+++ public/config/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const api = 'http://127.0.0.1:8080';
-export const serverHost = 'http://127.0.0.1:8008';
+const api = 'http://www.vol-4-ok.ru:8080';
+export const serverHost = 'http://www.vol-4-ok.ru:8008/';
 
 /**
  * Routes for API.
