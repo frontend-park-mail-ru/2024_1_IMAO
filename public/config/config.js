21a23c0f3bd19e915c6286972bf3485471545cb7
AlexeiLDD <doroga218@gmail.com>
2024-05-09 03:56:13 +0300

    80

--- public/config/config.js
+++ public/config/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const api = 'http://www.vol-4-ok.ru:3000';
-export const serverHost = 'http://www.vol-4-ok.ru:8008';
+const api = 'http://www.vol-4-ok.ru:8080';
+export const serverHost = 'http://www.vol-4-ok.ru:80';
 
 /**
  * Routes for API.
